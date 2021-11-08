167bd8d59d04eac3e205381b0bc048f7020ffa16
wwz-zackjo <zackjo@naver.com>
2021-11-08 14:27:06 +0900

    fix

--- src/infrastructure/axios.js
+++ src/infrastructure/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: 'http://www.jlventures.io/api',
+  baseURL: 'https://www.jlventures.io/api',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
